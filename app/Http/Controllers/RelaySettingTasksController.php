01bff472dae62e2ecc2d6efe59b9671eccef0299
AhmadAlharbi <ahmaadzaid7@gmail.com>
2023-11-30 21:34:42 +0300

    adding relay settings tasks table

---
+++ app/Http/Controllers/RelaySettingTasksController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RelaySettingTasks;
+use Illuminate\Http\Request;
+
+class RelaySettingTasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RelaySettingTasks  $relaySettingTasks
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RelaySettingTasks $relaySettingTasks)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RelaySettingTasks  $relaySettingTasks
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RelaySettingTasks $relaySettingTasks)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RelaySettingTasks  $relaySettingTasks
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RelaySettingTasks $relaySettingTasks)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RelaySettingTasks  $relaySettingTasks
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RelaySettingTasks $relaySettingTasks)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_11_30_212947_create_relay_setting_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relay_setting_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('status')->default(0);
+            $table->unsignedBigInteger('assigned_by');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('assigned_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('relay_setting_tasks');
+    }
+};
